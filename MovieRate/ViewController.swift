eb91b1eca747bea92fbd0c8cd843a528c20742a4
Dev Krishna Sadana <dev.krishna@navi.com>
2024-09-01 02:15:13 +0530

    Initial Commit

---
+++ MovieRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRate
+//
+//  Created by Dev Krishna Sadana on 01/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
